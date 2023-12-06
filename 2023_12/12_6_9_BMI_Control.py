4cd201f4e9b734b6a1e44f6c8332b701a0d1a7c0
Teodor.Georgiev <teodor98.georgiev@gmail.com>
2023-12-06 21:15:18 +0100

    simplification

--- 2023_12/12_6_9_BMI_Control.py
+++ 2023_12/12_6_9_BMI_Control.py
@@ -5,9 +5,9 @@ def BMI_calculation(weight,height):
     BMI = weight/height**2
     if BMI <= 18.4:
         return "eat more bread!!, you're underweight"
-    elif BMI > 18.4 and BMI <= 24.9:
+    elif BMI <= 24.9:
         return "all wright keep this weight, don't gorge more"
-    elif BMI > 25 and BMI <= 39.9:
+    elif BMI <= 39.9:
         return "well, correct your course, you're a bit overweight"
     else:
         return "are you seing yourself in mirror? what for a fat ball you become?"
@@ -18,3 +18,5 @@ insertion2 = float(input("enter your height here:"))
 
 verdict = BMI_calculation(insertion1,insertion2)
 print(verdict)
+
+#izlishna proverka - mahni
